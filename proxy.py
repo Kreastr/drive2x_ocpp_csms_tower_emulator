8ceeae7040a9cda77b8cfda6cc18e46f3d15f84e
Aleksei Romanenko <aleksei.romanenko@lut.fi>
2025-09-02 15:21:51 +0300

    sync version of ws to wss proxy

--- proxy.py
+++ proxy.py
@@ -1,46 +1,38 @@
-import asyncio
 import logging
+from threading import Thread
 
 import certifi
 from redis.connection import ssl
-from websockets.asyncio.server import serve, ServerConnection
-from websockets.asyncio.client import connect, ClientConnection
+from websockets.sync.server import serve, ServerConnection
+from websockets.sync.client import connect, ClientConnection
 
 logger = logging.getLogger(__name__)
 logger.setLevel(logging.DEBUG)
 
-
-
-
-async def proxify(ss):
+def proxify(ss):
     ctx = ssl.create_default_context(cafile=certifi.where())
 
-    async def _revtask(_ss : ServerConnection, _cs : ClientConnection):
+    def _revtask(_ss : ServerConnection, _cs : ClientConnection):
         while True:
-            _message = await _cs.recv()
+            _message = _cs.recv()
             logger.warning(f"C->S {_message}")
-            await _ss.send(_message)
-            await asyncio.sleep(1)
+            _ss.send(_message)
 
-    async with connect("wss://drive2x.lut.fi/ocpp/proxy/test/0001", ssl=ctx, subprotocols=["ocpp2.0.1"]) as cs:
-        revtask = asyncio.create_task(_revtask(ss, cs))
-        await asyncio.sleep(1)
+    with connect("wss://drive2x.lut.fi/ocpp/proxy/test/0001", ssl=ctx, subprotocols=["ocpp2.0.1"]) as cs:
+        revtask = Thread(target=lambda: _revtask(ss, cs))
+        revtask.start()
         while True:
             try:
-                message = await ss.recv()
+                message = ss.recv()
                 logger.warning(f"S->C {message}")
-                await cs.send(message)
-                await asyncio.sleep(1)
+                cs.send(message)
             except:
                 break
-        while revtask is None or not revtask.done():
-            await asyncio.sleep(1)
-        revtask.result()
+        revtask.join()
 
-async def main():
-    async with serve(proxify, "0.0.0.0", 9500) as server:
-        await server.serve_forever()
+def main():
+    with serve(proxify, "0.0.0.0", 9500) as server:
+        server.serve_forever()
 
-
-if __name__ == "__main__":
-    asyncio.run(main())
+if __name__ == '__main__':
+    main()
