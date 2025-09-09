8e5a646b25e5de61e377ebcd66e5a0df739e1715
Aleksei Romanenko <aleksei.romanenko@lut.fi>
2025-09-09 08:24:29 +0300

    hackfix EVSEStatus serialization

--- server/data/evse_status.py
+++ server/data/evse_status.py
@@ -1,15 +1,18 @@
+from attr.filters import exclude
 from nicegui import binding
 from ocpp.v201 import ChargePoint
 
 from util.types import ConnectorId, EVSEId, TransactionId
-from pydantic import BaseModel
+from pydantic import BaseModel, field_serializer, ConfigDict, Field
 
 
 class EvseStatus(BaseModel):
+    model_config = ConfigDict(arbitrary_types_allowed=True)
+    
     connector_id : ConnectorId = 0
     connector_status : str = "Unknown"
     timestamp : str = "1970.01.01"
     evse_id : EVSEId = -1
     tx_id : TransactionId = ""
-    cp_interface : ChargePoint | None = None
+    cp_interface : ChargePoint | None = Field(default=None, exclude=True)
     setpoint : float = 0.0
