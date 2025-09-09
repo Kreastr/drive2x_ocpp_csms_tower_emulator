1cf8906bebf69c2d610e6298036a4b0a764183fe
Aleksei Romanenko <aleksei.romanenko@lut.fi>
2025-09-09 07:47:49 +0300

    fix nicegui import

--- server/ui/nicegui.py
+++ server/ui/nicegui.py
@@ -4,22 +4,22 @@ import nicegui
 @dataclasses.dataclass
 class GUI:
     
-    _ui : nicegui.ui | None = None
-    _app : nicegui.app | None = None
-    _background_tasks : nicegui.background_tasks | None = None
+    _ui : type(nicegui.ui) | None = None
+    _app : type(nicegui.app) | None = None
+    _background_tasks : type(nicegui.background_tasks) | None = None
     
     @property
-    def ui(self) -> nicegui.ui:
+    def ui(self) -> type(nicegui.ui):
         assert self._ui is not None
         return self._ui
 
     @property
-    def app(self) -> nicegui.app:
+    def app(self) -> type(nicegui.app):
         assert self._app is not None
         return self._app
     
     @property
-    def background_tasks(self) -> nicegui.background_tasks:
+    def background_tasks(self) -> type(nicegui.background_tasks):
         assert self._background_tasks is not None
         return self._background_tasks
 
