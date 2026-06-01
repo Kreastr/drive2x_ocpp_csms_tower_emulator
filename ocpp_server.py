cf78c02731e2e5097bc5b3f6fbdbb133fb5725f0
Aleksei Romanenko <aleksei.romanenko@lut.fi>
2026-06-01 14:32:01 +0300

    explicit assignment to d2x ga3 demo group for lut charger

--- ocpp_server.py
+++ ocpp_server.py
@@ -247,8 +247,8 @@ EV_TAGS = {"IOW_LHH_": "iow_luccombe_hall_hotel",
            "PORTO_APT_": "porto_apt",
            "LatinkiHQ": "d2x_ga3_demo",
            "1132523008": "d2x_ga3_demo",
-           "1132523008": "d2x_ga3_demo",
-           "1132523017": "d2x_ga3_demo"
+           "1132523017": "d2x_ga3_demo",
+           "1132523027": "d2x_ga3_demo"
            }
 
 @app.post("/sca_data/setpoints")
